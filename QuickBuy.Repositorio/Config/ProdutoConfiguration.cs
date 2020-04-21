dc4c218ec459c4f4641653aec9d7fbfd1c0fd782
Alberto <alberto.doug@hotmail.com>
2020-04-21 00:49:43 -0300

    Repositorio e classes de mapeamento para DBContext

---
+++ QuickBuy.Repositorio/Config/ProdutoConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuickBuy.Dominio.Entidades;
+using System;
+
+namespace QuickBuy.Repositorio.Config
+{
+    public class ProdutoConfiguration : IEntityTypeConfiguration<Produto>
+    {
+
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
